a19092e8e88fecad9626357ca453288536ddc92e
Felix Simkovic <felixsimkovic@me.com>
2018-08-18 17:34:36 +0200

    initial setup for pytest

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import mock
+import os
+import pytest
+import sys
+
+os.environ['CCP4'] = os.environ['CCP4_SCR'] = '.'
+
+mock_modules = [
+    'cctbx',
+    'cctbx.crystal',
+    'cctbx.uctbx',
+    'cctbx.xray',
+    'iotbx',
+    'iotbx.pdb',
+    'iotbx.pdb.amino_acid_codes',
+    'iotbx.pdb.fetch',
+    'iotbx.reflection_file_utils',
+    'mmtbx',
+    'mmtbx.scaling',
+    'mmtbx.scaling.matthews',
+    'phaser',
+    'pyrvapi',
+]
+for module in mock_modules:
+    sys.modules[module] = mock.MagicMock()
